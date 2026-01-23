840d8503712645ad327ba32aaebfa65f5335c49f
Puujee42 <kayanokouji22@gmail.com>
2026-01-24 00:36:38 +0800

    try

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
   webpack: (config) => {
     config.experiments = {
       ...config.experiments,
